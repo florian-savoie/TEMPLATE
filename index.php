4d8e16e1f2511da6634949615b794b623babc0e5
florian-savoie <florianpsp303@gmail.com>
2023-09-26 22:37:19 +0200

    'message'

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-test template
+<?php 
+
+//$path = "C:\wamp64\www\TEMPLATE"; 
+$a='';
+//exec('cd ' . $path);
+exec("git add .");  
+exec("git commit -m'message'");
+echo "<h3 align = center> Succesfully commited all the files.</h3>";
+?>
